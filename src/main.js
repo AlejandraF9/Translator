b3415bd4634e484302d64e475a3f595da9ca56c3
Alejandra <alejandra.f.c.95@gmail.com>
2025-08-22 15:17:08 +0100

    feat: convert arrows const to button, style it and add click functionality to swap source/target languages including handling 'Detect language' option

--- src/main.js
+++ src/main.js
@@ -31,8 +31,19 @@ addOptions(originalLanguage, [
   "Italiano", "Portugués", "Ruso", "Japonés", "Chino"
 ]);
 
-const arrows = document.createElement("div");
+const arrows = document.createElement("button");
+arrows.setAttribute("class", "arrows");
 arrows.innerHTML = `<svg  xmlns="http://www.w3.org/2000/svg"  width="28"  height="28"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon-tabler icons-tabler-outline icon-tabler-mobiledata"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16 12v-8" /><path d="M8 20v-8" /><path d="M13 7l3 -3l3 3" /><path d="M5 17l3 3l3 -3" /></svg>`;
+
+arrows.addEventListener("click", () => {
+  const change = originalLanguage.value;
+  originalLanguage.value = translatedLanguage.value;
+  translatedLanguage.value = change;
+
+  if(translatedLanguage.value === "") {
+    translatedLanguage.value = "Español";
+  }
+});
 
 const translatedLanguage = document.createElement("select");
 translatedLanguage.textContent = "Inglés";
